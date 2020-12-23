294274441422855cd8d17e0d616dc1169f3421ee
Glitch (granite-wild-football) <none>
2020-12-23 17:06:55 +0000

    🍁👡 Checkpoint ./komutlar/isim-değiştir.js:26119612/13

--- komutlar/isim-değiştir.js
+++ komutlar/isim-değiştir.js
@@ -18,7 +18,7 @@ exports.run = async (client, message, args) => {
 exports.conf = {
   enabled: true,
   guildOnly: true,
-  aliases: ['isim'],
+  aliases: ['isim','nick'],
   permLevel: 0
 };
 exports.help = {
